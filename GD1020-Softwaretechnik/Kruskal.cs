9324d9fd97d0652593d0537c25e6d489513a40bf
Jan <jan@atrax.de>
2022-03-11 12:50:18 +0100

    this

--- GD1020-Softwaretechnik/Kruskal.cs
+++ GD1020-Softwaretechnik/Kruskal.cs
@@ -11,6 +11,119 @@ namespace GD1020_Softwaretechnik
     {
         private static void Main(string[] args)
         {
+            UndirectedGraphStructure graphStructure = new UndirectedGraphStructure(7);
+
+            graphStructure.AddEdge(0, 1, 7);
+            graphStructure.AddEdge(0, 3, 5);
+            graphStructure.AddEdge(1, 2, 8);
+            graphStructure.AddEdge(1, 3, 9);
+            graphStructure.AddEdge(2, 4, 5);
+            graphStructure.AddEdge(3, 4, 15);
+            graphStructure.AddEdge(3, 5, 6);
+            graphStructure.AddEdge(4, 5, 8);
+            graphStructure.AddEdge(4, 6, 9);
+            graphStructure.AddEdge(5, 6, 11);
+
+            ExecuteKruskal(graphStructure);
+
+            Console.WriteLine(graphStructure.ToString());
+            Console.ReadKey();
+        }
+
+        public static UndirectedGraphStructure ExecuteKruskal(UndirectedGraphStructure graphStructure)
+        {
+            return MarkShortest(graphStructure);
+        }
+
+        private static UndirectedGraphStructure MarkShortest(UndirectedGraphStructure graphStructure)
+        {
+            Dictionary<int, List<int>> shortestTuples = new Dictionary<int, List<int>>(graphStructure.Edge);
+            for (int i = 0; i < graphStructure.Edge; i++)
+            {
+                shortestTuples.Add(i, new List<int>());
+            }
+
+            int currentWeight = Int32.MaxValue;
+
+            for (int i = 0; i < graphStructure.Adjacent.Length; i++)
+            {
+                for (int j = 0; j < graphStructure.Adjacent[i].Count; j++)
+                {
+                    if (graphStructure.Adjacent[i][j].isMarked == false)
+                    {
+                        if (graphStructure.Adjacent[i][j].weight < currentWeight)
+                        {
+                            currentWeight = graphStructure.Adjacent[i][j].weight;
+                            foreach (KeyValuePair<int, List<int>> shortestTuple in shortestTuples)
+                            {
+                                shortestTuple.Value.Clear();
+                            }
+
+                            shortestTuples[i].Add(graphStructure.Adjacent[i][j].connection);
+                        }
+                        else if (graphStructure.Adjacent[i][j].weight < currentWeight)
+                        {
+                            shortestTuples[i].Add(graphStructure.Adjacent[i][j].connection);
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < shortestTuples.Count; i++)
+            {
+                for (int j = 0; j < shortestTuples[i].Count; j++)
+                {
+                    if (IsCyclic(graphStructure, i, shortestTuples[i][j], i, new List<int>()))
+                    {
+                        graphStructure.Adjacent[i].Remove((j, currentWeight, false));
+                        graphStructure.RemoveEdge();
+                    }
+                    else
+                    {
+                        graphStructure.Adjacent[i].Remove((j, currentWeight, false));
+                        graphStructure.Adjacent[i].Add((j, currentWeight, true));
+                    }
+                }
+            }
+
+            for (int i = 0; i < graphStructure.Adjacent.Length; i++)
+            {
+                for (int j = 0; j < graphStructure.Adjacent[i].Count; j++)
+                {
+                    if(graphStructure.Adjacent[i][j].isMarked == false)
+                        return MarkShortest(graphStructure);
+                }
+            }
+
+            return graphStructure;
+        }
+
+        private static bool IsCyclic(UndirectedGraphStructure graphStructure, int vertexA, int vertexB, int startingVertex, List<int> connectionList)
+        {
+            if (startingVertex == vertexA)
+            {
+                foreach (int i in connectionList)
+                {
+                    if (i == vertexB)
+                        return true;
+                }
+
+                return false;
+            }
+
+            startingVertex += 1;
+
+            foreach ((int connection, int weight, bool isMarked) valueTuple in graphStructure.Adjacent[startingVertex])
+            {
+                if(valueTuple.isMarked)
+                    connectionList.Add(valueTuple.connection);
+            }
+
+            return IsCyclic(graphStructure, vertexA, vertexB, startingVertex, connectionList);
+        }
+
+        /*private static void Main(string[] args)
+        {
             GraphStructure graphStructure = new GraphStructure(new Dictionary<int, List<(int weight, int connectedVertex)>>(), new List<GraphStructure.Vertex>());
 
             graphStructure.InsertMultipleVertices(new []{0,1,2,3,4,5,6});
@@ -94,7 +207,7 @@ namespace GD1020_Softwaretechnik
 
                 }
             }
-            
+
             if (graphStructure.ConnectionDictionary.Count > 0)
                 return FindShortest(graphStructure, maxGraphStructure);
 
@@ -153,6 +266,6 @@ namespace GD1020_Softwaretechnik
             }
 
             return IsCyclic(maxGraphStructure,vertexA,vertexB,startingVertex, connectionList);
-        }
+        }*/
     }
 }
---
+++ GD1020-Softwaretechnik/UndirectedGraphStructure.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD1020_Softwaretechnik
+{
+    public class UndirectedGraphStructure
+    {
+        private readonly int _vertex;
+        private int _edge;
+
+        private List<(int connection, int weight, bool isMarked)>[] _adjacent;
+
+        public UndirectedGraphStructure(int vertex)
+        {
+            _vertex = vertex;
+
+            _adjacent = new List<(int connection, int weight, bool isMarked)>[vertex];
+
+            for (int i = 0; i < _adjacent.Length; i++)
+                _adjacent[i] = new List<(int connection, int weight, bool isMarked)>();
+        }
+
+        public List<(int connection, int weight, bool isMarked)>[] Adjacent
+        {
+            get => _adjacent;
+            private set { }
+        }
+
+        public int Vertex 
+        {
+            get => _vertex;
+            private set { }
+        }
+
+        public int Edge
+        {
+            get => _edge;
+            private set { }
+        }
+
+        public void AddEdge(int edgeA, int edgeB, int weight)
+        {
+            if (edgeA > _adjacent.Length || edgeB > _adjacent.Length)
+                throw new ArgumentException("Invalid node number specified.");
+
+            _adjacent[edgeA].Add((edgeB, weight, false));
+            _adjacent[edgeB].Add((edgeA, weight, false));
+            _edge++;
+        }
+
+        public List<(int, int, bool)> GetAdjacency(int vertex)
+        {
+            return _adjacent[vertex];
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine(_vertex + " vertices, " + _edge + " edges");
+            for (int i = 0; i < _vertex; i++)
+            {
+                builder.Append(i + ": ");
+                foreach (var connection in _adjacent[i])
+                {
+                    builder.Append("(Connection: " + connection.connection + " Weight: " + connection.weight + ") ");
+                }
+                builder.AppendLine(string.Empty);
+            }
+
+            return builder.ToString();
+        }
+
+        public void MarkEdge(int edgeA, int edgeB)
+        {
+            for (int i = 0; i < _adjacent[edgeA].Count; i++)
+            {
+                if (_adjacent[edgeA][i].connection == edgeB)
+                {
+                }
+            }
+        }
+
+        public void RemoveEdge()
+        {
+            Edge -= 1;
+        }
+    }
+}
+
